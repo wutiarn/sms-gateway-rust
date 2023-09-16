f1d1218051d9c3b45c04ddf7a9d9a78fbf5c446a
Dmitry Romanov <me@wtrn.ru>
2023-09-16 13:34:19 +0300

    WIP: Migrate to new SmsMessageDto

--- src/dto/sms_message.rs
+++ src/dto/sms_message.rs
@@ -1,26 +1,30 @@
-use lazy_static::lazy_static;
-use rocket::data::FromData;
 use serde::{Deserialize, Serialize};
-use time::format_description;
-use time::format_description::FormatItem;
-
-const DELIMITER: &str = "^~";
-
-lazy_static! {
-    static ref DATETIME_FORMAT: Vec<FormatItem<'static>> = format_description::parse("[day].[month].[year] [hour].[minute]").unwrap();
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all="camelCase")]
 pub struct SmsMessagesDto {
     pub device_id: String,
-    pub career_name: String,
-    pub messages: Vec<SmsMessagesDto>,
+    pub carrier_name: String,
+    pub messages: Vec<SmsMessageDto>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
 pub struct SmsMessageDto {
     pub from: String,
     pub message: String,
     pub received_at: u64
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::dto::SmsMessagesDto;
+
+    #[test]
+    fn test_deserialization() {
+        let json = "{\"deviceId\": \"8200195eb5e476d0\", \"carrierName\": \"MGTS\", \"messages\": [{\"from\": \"MTS-Bank\", \"message\": \"Oplata 719,92 RUB OKEY Ostatok: 253 373,18 RUB; *0530 \", \"receivedAt\": 1690554221000}]}";
+        let dto: SmsMessagesDto = serde_json::from_str(json).unwrap();
+        assert_eq!(dto.device_id, "8200195eb5e476d0");
+        assert_eq!(dto.carrier_name, "MGTS");
+    }
 }
